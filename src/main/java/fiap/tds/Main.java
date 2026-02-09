fd23c98119c74d3fa3dab60985d31c95ecf03f84
lleonardogr <lleonardogr@gmail.com>
2026-02-09 17:24:35 -0300

    feat: Extend Account to have a ledger

--- src/main/java/fiap/tds/Main.java
+++ src/main/java/fiap/tds/Main.java
@@ -10,8 +10,8 @@ long saldo = 0;
 void main(){
     IO.println("Sistema do CaixaVerso: depósito <v>, Saques <v>, Saldo e Sair");
     var newAcc = new Account(AccountId.newId(), "Léo", Money.zero(), Money.parseSigned("1000.00"));
-    newAcc.apply(new Deposit(TransactionId.newId(), Instant.now(),
-            Money.parsePositive("10.00"), "PIX", "pagamento de salário"));
-    newAcc.apply(new Withdrawl(TransactionId.newId(), Instant.now(), Money.parsePositive("3.50"), "SAQUE", "pagamento de contas"));
+    //newAcc.apply(new Deposit(TransactionId.newId(), Instant.now(),
+            //Money.parsePositive("10.00"), "PIX", "pagamento de salário"));
+    //newAcc.apply(new Withdrawl(TransactionId.newId(), Instant.now(), Money.parsePositive("3.50"), "SAQUE", "pagamento de contas"));
     IO.println("saldo=" + newAcc.balance());
 }
--- src/main/java/fiap/tds/entities/account/Account.java
+++ src/main/java/fiap/tds/entities/account/Account.java
@@ -1,7 +1,13 @@
 package fiap.tds.entities.account;
 
+import fiap.tds.entities.transaction.Deposit;
 import fiap.tds.entities.transaction.Transaction;
+import fiap.tds.entities.transaction.TransactionId;
+import fiap.tds.entities.transaction.Withdrawl;
 import fiap.tds.entities.valueObjects.Money;
+
+import java.time.Instant;
+import java.util.*;
 
 import static java.util.Objects.requireNonNull;
 
@@ -11,6 +17,9 @@ public final class Account {
     private final Money overdraftLimit;
 
     private Money balance;
+
+    private final List<Transaction> history = new ArrayList<>();
+    private final Map<TransactionId, Transaction> byId = new HashMap<>();
 
     public Account(AccountId id, String owner, Money initialBalance, Money overdraftLimit) {
         this.id = requireNonNull(id);
@@ -28,10 +37,47 @@ public final class Account {
     public Money balance(){return balance;}
     public Money overdraftLimit(){return overdraftLimit;}
 
-    public void apply(Transaction t){
+    public TransactionId deposit(String amount, String category, String description){
+        var tx = new Deposit(TransactionId.newId(), Instant.now(), Money.parsePositive(amount), category, description);
+        apply(tx);
+        return tx.id();
+    }
+
+    public TransactionId withdraw(String amount, String category, String description) {
+        var tx = new Withdrawl(TransactionId.newId(), Instant.now(), Money.parsePositive(amount), category, description);
+        apply(tx);
+        return tx.id();
+    }
+
+    public Optional<Transaction> find(Transaction id){
+        return Optional.ofNullable(byId.get(id));
+    }
+
+    List<Transaction> last(int n){
+        int size = history.size();
+        int from = Math.max(0, size - n);
+        return List.copyOf(history.subList(from, size));
+    }
+
+    Map<String, Money> sumByCategory(){
+        var sums = new HashMap<String, Money>();
+        for(var t: history)
+            sums.merge(t.category(), t.delta(), Money::plus);
+        return  Map.copyOf(sums);
+    }
+
+    private void apply(Transaction t){
+        requireNonNull(t);
+
+        if(byId.containsKey(t.id()))
+            throw new IllegalArgumentException("transação dupicada, id=" + t.id());
+
         var newBalance = balance.plus(t.delta());
         if(newBalance.compareTo(overdraftLimit.negate()) < 0)
             throw new IllegalArgumentException("Saldo insuficiente para realizar a transação (limite estourado)");
         balance = newBalance;
+
+        history.add(t);
+        byId.put(t.id(), t);
     }
 }
